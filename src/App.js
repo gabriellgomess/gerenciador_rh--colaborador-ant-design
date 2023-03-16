40822d2b7cc23921a188a9e3f468d692012d66b6
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2023-03-16 17:17:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { FileOutlined, PieChartOutlined, UserOutlined, DesktopOutlined, TeamOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { useState } from 'react';
+const { Header, Content, Footer, Sider } = Layout;
 
-function App() {
+const items = [
+  {
+    label: 'Dashboard',
+    key: '1',
+    icon: <PieChartOutlined />,
+  },
+  {
+    label: 'Usuários',
+    key: '2',
+    icon: <UserOutlined />,
+  },
+  {
+    label: (
+      <a href="#" target="_blank" rel="noopener noreferrer">
+        Clientes
+      </a>
+    ),
+    key: '3',
+    icon: <DesktopOutlined />,
+  },
+ 
+];
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div
+          style={{
+            height: 32,
+            margin: 16,
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+        
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
   );
-}
-
+};
 export default App;
