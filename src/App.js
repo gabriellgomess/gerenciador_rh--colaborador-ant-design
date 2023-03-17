b195017326da1fb99e82e3c395c39e2fc5da0daa
gabriellgomess <51470523+gabriellgomess@users.noreply.github.com>
2023-03-16 22:10:10 -0300

    upload file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { FileOutlined, PieChartOutlined, UserOutlined, DesktopOutlined, TeamOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import { useState } from 'react';
+import Perfil from './Pages/Perfil';
 const { Header, Content, Footer, Sider } = Layout;
 
 const items = [
@@ -59,14 +60,7 @@ const App = () => {
             margin: '0 16px',
           }}
         >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
+          
           <div
             style={{
               padding: 24,
@@ -74,7 +68,7 @@ const App = () => {
               background: colorBgContainer,
             }}
           >
-            Bill is a cat.
+            <Perfil />
           </div>
         </Content>
         <Footer
